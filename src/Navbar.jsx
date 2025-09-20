197872ce5604fcd5d313eb10570c66eb0b3a50fa
York Yu <n11453559@gmail.com>
2025-09-20 22:04:26 +1000

    refactor: simplify toggleLinks function in Navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,18 +6,32 @@ import logo from './logo.svg'
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
 
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="logo" alt="logo" />
-          <button
-            className="nav-toggle"
-            onClick={() => setShowLinks(!showLinks)}
-          >
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
+        {showLinks && (
+          <div className={'links-container'}>
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   )
